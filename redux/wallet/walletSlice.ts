995e2cee71195b2a64a8ad913d3852e284e0265e
unknown <daudakolo16@gmail.com>
2025-01-20 13:05:06 +0100

    global state/updating redux

--- redux/wallet/walletSlice.ts
+++ redux/wallet/walletSlice.ts
@@ -4,15 +4,31 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 type initialStateType = {
   walletType: AvailableWallets | null;
   address: string | null;
+  keplrOfflineSigner?: any;
 };
 const initailState: initialStateType = {
   walletType: null,
   address: null,
+  keplrOfflineSigner: undefined,
 };
 const walletSlice = createSlice({
   name: "wallet",
   initialState: initailState,
   reducers: {
+    connectGlobalWallet: (
+      state,
+      {
+        payload,
+      }: PayloadAction<{
+        address: string | null;
+        type: AvailableWallets;
+        keplrOfflineSigner?: any;
+      }>
+    ) => {
+      state.walletType = payload.type;
+      state.address = payload.address;
+      state.keplrOfflineSigner = payload.keplrOfflineSigner;
+    },
     updateWalletType: (state, action: PayloadAction<AvailableWallets>) => {
       state.walletType = action.payload;
     },
@@ -25,6 +41,10 @@ const walletSlice = createSlice({
   },
 });
 
-export const { updateWalletType, updateAddress, removeAddress } =
-  walletSlice.actions;
+export const {
+  connectGlobalWallet,
+  updateWalletType,
+  updateAddress,
+  removeAddress,
+} = walletSlice.actions;
 export default walletSlice.reducer;
